228a8cde9d8d7d99fda19276847b72cce8b4f976
furby™ <devs@wabi.foundation>
2023-07-03 23:48:38 -0500

    fix ctor.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/Wabi-Studios/STTextView.git",
-      exact: "0.7.3"
+      exact: "0.7.4"
     ),
     .package(
       url: "https://github.com/CodeEditApp/CodeEditLanguages.git",
