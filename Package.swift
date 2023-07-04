d991c497f062f915b2b81efad9c590c5cfb8fca2
furby™ <devs@wabi.foundation>
2023-07-03 23:33:04 -0500

    Bump STTextView.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/Wabi-Studios/STTextView.git",
-      exact: "0.7.1"
+      exact: "0.7.2"
     ),
     .package(
       url: "https://github.com/CodeEditApp/CodeEditLanguages.git",
